7451c45daf6099275e13a1b51c16ce8ea05cae65
Actory Ou <ousc@amazon.com>
2021-12-03 05:22:35 +0000

    replace strnlen with memchr

---
+++ test/cbmc/stubs/memchr.c
@@ -0,0 +1,53 @@
+/*
+ * FreeRTOS-Cellular-Interface v1.1.0
+ * Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * https://www.FreeRTOS.org
+ * https://github.com/FreeRTOS
+ */
+
+/**
+ * @file strtok.c
+ * @brief Creates a stub for strtok. This stub checks if, for the input copy
+ * length, the destination and source are valid accessible memory.
+ */
+
+#include <stdio.h>
+
+void *memchr( void * ptr, 
+              int value, 
+              size_t num )
+{
+    //to avoid unwind assertion on CBMC, use a constant loop time to enhance CBMC performance
+    int n = CBMC_MAX_BUFSIZE;
+    char *p = (char*)ptr;
+
+    while( n-- && p!=NULL )
+    {
+        if( *p == (char)value )
+        {
+            return p;
+        }
+        p++;
+    }
+
+    return NULL;
+}
+
