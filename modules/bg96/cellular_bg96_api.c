fd6165415d6013ed4f63a9287f97a6fcb16699d5
Soren Ptak <Skptak@outlook.com>
2022-09-22 10:59:18 -0700

    Version Number Fix (#104)
    * Updating version number for LTS 2.0 release. Added relevant PRs to the CHANGELOG
    
    * Fixing pattern match type in README
    
    Co-authored-by: Soren Ptak <skptak@amazon.com>

--- modules/bg96/cellular_bg96_api.c
+++ modules/bg96/cellular_bg96_api.c
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS-Cellular-Interface v1.2.0
+ * FreeRTOS-Cellular-Interface v1.3.0
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
