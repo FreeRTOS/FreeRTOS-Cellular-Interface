c3a6916892b82fd942a81b1143316e5b49eb25c8
diogosilva1 <diogosilva@stratioautomotive.com>
2023-08-29 17:40:26 +0000

    Address formatting rules

--- source/include/cellular_api.h
+++ source/include/cellular_api.h
@@ -650,10 +650,10 @@ CellularError_t Cellular_SocketRegisterClosedCallback( CellularHandle_t cellular
  * @return CELLULAR_SUCCESS if the operation is successful, otherwise an error
  * code indicating the cause of the error.
  */
-CellularError_t Cellular_ConfigureSSLContext(CellularHandle_t cellularHandle,
-                                      uint8_t sslContextId,
-                                      const char* sslConfigurationParameter,
-                                      const char* inputArg);
+CellularError_t Cellular_ConfigureSSLContext( CellularHandle_t cellularHandle,
+                                              uint8_t sslContextId,
+                                              const char* sslConfigurationParameter,
+                                              const char* inputArg );
 
 /**
  * @brief Upload a File into the Storage.
@@ -669,12 +669,11 @@ CellularError_t Cellular_ConfigureSSLContext(CellularHandle_t cellularHandle,
  * @return CELLULAR_SUCCESS if the operation is successful, otherwise an error
  * code indicating the cause of the error.
  */
-CellularError_t Cellular_UploadFileToStorage(CellularHandle_t cellularHandle, 
-                                    const char* filename,
-                                    const char* fileContent,
-                                    uint32_t fileSize,
-                                    uint32_t* pSentDataLength);      
-                                                 
+CellularError_t Cellular_UploadFileToStorage( CellularHandle_t cellularHandle,
+                                              const char* filename,
+                                              const char* fileContent,
+                                              uint32_t fileSize,
+                                              uint32_t* pSentDataLength );
 
 /* *INDENT-OFF* */
 #ifdef __cplusplus
